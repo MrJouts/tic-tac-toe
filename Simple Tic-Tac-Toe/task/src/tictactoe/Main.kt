c7753b921b7201e8aba2ea1f99b7eb1a31a6749c
Emiliano <emiliano.hotes@despegar.com>
2022-11-23 16:09:07 -0300

    Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,7 +1,11 @@
 package tictactoe
 
 fun main() {
-    println("X O X")
-    println("O X O")
-    println("X X O")
+    val d = readln()
+    println("---------")
+    println("| ${d[0]} ${d[1]} ${d[2]} |")
+    println("| ${d[3]} ${d[4]} ${d[5]} |")
+    println("| ${d[6]} ${d[7]} ${d[8]} |")
+    println("---------")
+
 }
